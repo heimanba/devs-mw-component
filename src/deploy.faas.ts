9655e6c8b4c0973338249d68425cb752b1ca0bc4
yuankun <yuankun.yk@alibaba-inc.com>
2021-03-26 10:39:51 +0800

    feat: deploy部署工具完成

--- src/deploy.faas.ts
+++ src/deploy.faas.ts
@@ -3,13 +3,14 @@ import { CommandCore } from '@midwayjs/command-core';
 import { loadSpec } from '@midwayjs/serverless-spec-builder';
 import { PackagePlugin } from '@midwayjs/fcli-plugin-package';
 import { AliyunFCPlugin } from '@midwayjs/fcli-plugin-fc';
+import { DeployPlugin } from '@midwayjs/fcli-plugin-deploy';
 
 const deployFaas = async (baseDir: string) => {
   const core = new CommandCore({
     config: {
       servicePath: baseDir,
     },
-    commands: ['package', 'deploy'],
+    commands: ['deploy'],
     service: loadSpec(baseDir),
     provider: 'aliyun',
     log: console,
@@ -20,6 +21,7 @@ const deployFaas = async (baseDir: string) => {
   });
   core.addPlugin(PackagePlugin);
   core.addPlugin(AliyunFCPlugin);
+  core.addPlugin(DeployPlugin);
   await core.ready();
   await core.invoke(['deploy']);
 };
