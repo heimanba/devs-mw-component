8da437d9b3b3a50549b9995f6dd231bbab59743d
yuankun <yuankun.yk@alibaba-inc.com>
2021-03-30 14:08:17 +0800

    feat: add dev 命令

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,23 @@ import build from './build.faas';
 
 export default class JamStackComponent {
   @HLogger('MIDWAY_FAAS') logger: ILogger;
+  /**
+   * 部署应用
+   * @param inputs
+   */
   async deploy(inputs: any) {
-    const { ProjectName } = inputs.Project;
+    const { ProjectName } = inputs.Project || inputs.props;
     const { cwd } = process;
     this.logger.debug(`[${ProjectName}] inputs params: ${JSON.stringify(inputs, null, 2)}`);
-
     await build(cwd());
-
     await deploy(cwd());
     this.logger.info('部署完成');
+  }
+  async dev(inputs: any) {
+    const { ProjectName } = inputs.Project;
+    this.logger.debug(`[${ProjectName}] inputs params: ${JSON.stringify(inputs, null, 2)}`);
+    const { faasConfig } = inputs.Properties || inputs.props;
+    console.log(faasConfig);
+    // faasConfig => midway.config.js
   }
 }
